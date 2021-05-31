dd00d934d69208f66bf7557c56a22329595cf5ec
Niklas Sauter <niklas@n1ks.net>
2021-05-31 09:02:17 +0200

    guix: config-vps.scm: Fix coturn configuration

--- config-vps.scm
+++ config-vps.scm
@@ -67,17 +67,17 @@
                              (call-with-input-file
                                "data/coturn-auth-secret.txt"
                                get-string-all))
-              "server-name=turn.n1ks.net"
-              "realm=turn.n1ks.net"
-              "cert=/etc/letsencrypt/live/turn.n1ks.net/fullchain.pem"
-              "pkey=/etc/letsencrypt/live/turn.n1ks.net/privkey.pem"
-              "external-ips=78.47.91.233"
-              "denied-peer-ips=10.0.0.0-10.255.255.255"
-              "denied-peer-ips=192.168.0.0-192.168.255.255"
-              "denied-peer-ips=172.16.0.0-172.31.255.255"
-              "allowed-peer-ips=10.0.0.1"
-              "user-quota=12"
-              "total-quota=1200"))))
+             "server-name=turn.n1ks.net"
+             "realm=turn.n1ks.net"
+             "cert=/etc/letsencrypt/live/turn.n1ks.net/fullchain.pem"
+             "pkey=/etc/letsencrypt/live/turn.n1ks.net/privkey.pem"
+             "external-ip=78.47.91.233"
+             "denied-peer-ip=10.0.0.0-10.255.255.255"
+             "denied-peer-ip=192.168.0.0-192.168.255.255"
+             "denied-peer-ip=172.16.0.0-172.31.255.255"
+             "allowed-peer-ip=10.0.0.1"
+             "user-quota=12"
+             "total-quota=1200"))))
       (service certbot-service-type
         (certbot-configuration
           (email "niklas@n1ks.net")
